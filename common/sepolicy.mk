229870cb11d12d739a1cc30dc0129d1e72f6ba15
Rizky Benggolo <travarilo@gmail.com>
2021-08-31 14:51:19 +0000

    sepolicy: [SQUASH] Revert flipendo rules
    Revert "sepolicy: move and guard access to power HAL"
    
    This reverts commit 0d6657a8dce4b51207f1a49ea3fc9121ec736135.
    
    Revert "sepolicy: move flipendo to dynamic"
    
    This reverts commit 5a1aff116dbe3bef693da98684ae958e823bac6c.
    
    Revert "sepolicy: Add permissions for Flipendo to PowerHAL"
    
    This reverts commit 82bf536189fe341ae4df5189add1586dc9a2d30f.
    
    Revert "sepolicy: Add Flipendo rules"
    
    This reverts commit 9f82e14cb4ccfd15c807717828f75577c041378d.

--- common/sepolicy.mk
+++ common/sepolicy.mk
@@ -25,15 +25,9 @@ ifeq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
     device/banana/sepolicy/common/dynamic \
     device/banana/sepolicy/common/system
-
-ifneq ($(TARGET_HAL_POWER_RW_INPUT_DEVICE), true)
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
-    device/custom/sepolicy/common/dynamic_extra
-endif
 else
 BOARD_VENDOR_SEPOLICY_DIRS += \
     device/banana/sepolicy/common/dynamic \
-    device/custom/sepolicy/common/dynamic_extra \
     device/banana/sepolicy/common/vendor
 endif
 
